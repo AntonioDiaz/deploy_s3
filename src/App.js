515ac72d04ebb1a5456037d9c00ba88f18eeb32b
Antonio Diaz Arroyo <antonio.diaz.arroyo@gmail.com>
2020-12-08 19:03:22 +0100

    add bulma libray

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Welcome campeonato mus
-      </header>
+      <h2 className="title">Campeonato de mus 2020 - Memorial "Los Sordos"</h2>
     </div>
   );
 }
-
 export default App;

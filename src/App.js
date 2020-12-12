94f37d713d49d4570945422839903e2959b94749
Antonio Diaz Arroyo <antonio.diazarroyo@keytree.es>
2020-12-12 17:25:31 +0100

    create main pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import 'bulma/css/bulma.css'
+import {Switch, Route} from 'react-router-dom'
+import {NotFound} from './pages/NotFound'
+import {Dashboard} from './pages/Dashboard'
+import {Init} from './pages/Init'
 
 function App() {
+  const url = new URL(document.location)
+  console.log("*url -> " + JSON.stringify(url))
+  console.log("search params -> " + url.searchParams)
   return (
     <div className="App">
-      <h2 className="title">Campeonato de mus 2020 - Memorial "Los Sordos"</h2>
+      <Switch>
+        <Route exact path='/' component={Dashboard} ></Route>
+        <Route exact path='/init' component={Init} ></Route>
+        <Route component={NotFound}></Route>      
+      </Switch>        
     </div>
   );
 }
+
 export default App;
